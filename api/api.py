76bb72d945b6e1dc425ca15fad9cf99204c9efe3
KabigonManiac <162381907+KabigonManiac@users.noreply.github.com>
2025-01-06 20:51:48 +0900

    db 병합 적용 이전 정리

--- api/api.py
+++ api/api.py
@@ -9,8 +9,7 @@ from data_class.request import RequestData
 from langfuse.decorators import observe
 
 from graph.graph import make_graph
-from fuse.langfuse_handler import langfuse_handler
-from fuse.langfuse_client import LangfuseClient
+from utils.langfuse_handler import langfuse_handler
 
 # 디버깅용 import
 from datetime import datetime
@@ -29,9 +28,6 @@ class Feedback(BaseModel):
 
 api = APIRouter(tags=["api"])
 graph = make_graph()
-
-langfuse_client = LangfuseClient(base_url="http://localhost:8001")
-
 
 @api.post("/process")
 async def process_input(request: RequestData):
@@ -62,58 +58,3 @@ async def process_input(request: RequestData):
         }  # trace_id도 반환
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error processing input: {str(e)}")
-
-import json
-from ML.classification import classifier
-from ML.regression import regressor
-
-@api.post("/classification")
-async def process_classification(request: RequestData):
-    """사용자 질문에 대해 분류 분석 결과를 반환하는 엔드포인트"""
-    try:
-        result = await classifier.analyze_question(request.user_question)
-        
-        combined_result = {
-            "answer": "class" + result["answer"],
-            "raw_data": [result["raw_data"]]
-        }
-        
-        final_result = {
-            "status": "success",  # 나중에 제거
-            "result": {
-                "answer": json.dumps(combined_result, ensure_ascii=False),
-                "table": []
-            },
-            "raw_data": [],  # 나중에 수정
-            "trace_id": "100000"  # 나중에 수정
-        }
-        
-        return final_result
-    except Exception as e:
-        raise HTTPException(
-            status_code=500,
-            detail=f"Error in classification process: {str(e)}"
-        )
-
-@api.post("/regression")
-async def process_regression(request: RequestData):
-    """사용자 질문에 대해 회귀 분석 결과를 반환하는 엔드포인트"""
-    try:
-        result = await regressor.analyze_question(request.user_question)
-        print(result.keys())
-        print(result['data'])
-        col = list(result['data'][0].keys())
-        print(col)
-        final_result = {
-           "status": "success",  # 나중에 제거
-           "result": {"answer": result['answer'], "table":col},  # 나중에 수정
-           "raw_data": result['data'],
-           "trace_id": '100000',  # 나중에 수정
-        }
-        return final_result
-    except Exception as e:
-        print(e)
-        raise HTTPException(
-            status_code=500,
-            detail=f"Error in regression process: {str(e)}"
-        )
