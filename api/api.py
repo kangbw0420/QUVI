bcdf63c264e4a0d19657d09aae5984c84146c81d
lee.jihyeong <lee.jihyeong0823@gmail.com>
2025-01-07 17:15:18 +0900

    api 수정
    last data 사용 및 session_id 사용에 따른 api수정
    request, response 전부 수정

--- api/api.py
+++ api/api.py
@@ -1,25 +1,31 @@
-import sys
 import uuid
 import traceback
 
-import pandas as pd
 from pydantic import BaseModel
 from fastapi import APIRouter, HTTPException
 from data_class.request import RequestData
-from langfuse.decorators import observe
+from typing import Optional
 
 from graph.graph import make_graph
-from fuse.langfuse_handler import langfuse_handler
-from fuse.langfuse_client import LangfuseClient
+
 
 # 디버깅용 import
 from datetime import datetime
 
-
+class Output(BaseModel):
+    status: int# 200
+    success: bool# True
+    retCd: int # 200 
+    message: str# 질답 성공
+    body: dict # 본문
+    
 class Input(BaseModel):
     user_question: str
-    user_id: str = "default_user"  # 사용자 식별을 위한 기본값
-
+    user_id: str = 'default_user'
+    session_id:str = 'default_session'
+    last_question: Optional[str]  # 이전 그래프의 사용자 질문
+    last_answer: Optional[str]    # 이전 그래프의 LLM 답변
+    last_sql_query: Optional[str] # 이전 그래프의 SQL 쿼리
 
 class Feedback(BaseModel):
     trace_id: str
@@ -30,37 +36,49 @@ class Feedback(BaseModel):
 api = APIRouter(tags=["api"])
 graph = make_graph()
 
-langfuse_client = LangfuseClient(base_url="http://localhost:8001")
-
 
 @api.post("/process")
-async def process_input(request: RequestData):
+async def process_input(request: Input) -> Output:
     """프로덕션용 엔드포인트"""
     try:
-        # 트레이스 ID 생성
-        trace_id = str(uuid.uuid4())
-
+        # 트레이스 ID 생성 (langfuse)
+        # trace_id = str(uuid.uuid4())
+        # print("request:", request.user_question)
         data = await graph.ainvoke(
-            {"user_question": request.user_question},
-            config={"callbacks": [langfuse_handler]},
+            {"user_question": request.user_question,
+             "last_question": request.last_question,
+             "last_answer": request.last_answer,
+             "last_sql_query": request.last_sql_query,
+            }#,
+            #config={"callbacks": [langfuse_handler]},
         )
-        # result
+        # print("data:",data)
+        # 본문 {"result": {"answer": string, "table": []}, "raw_data": [], "SQL": ""}
+        question = data["analyzed_question"]
+        # result - answer
         answer = data["final_answer"]
-
         # raw_data
         raw_data = data["query_result"]
-        col = list(raw_data[0].keys())
-        print(col)
-        result = {"answer": answer, "table":col}
-
-
-        return {
-            "status": "success",  # 이 부분은 나중에 제거 필요
-            "result": result,
-            "raw_data": raw_data,
-            "trace_id": trace_id,
-        }  # trace_id도 반환
+        # result - table()
+        try:
+            table = list(raw_data[0].keys())
+        except IndexError as E:
+            table = []
+        # result {answer, table}
+        result = {"question" :question,"answer": answer, "table":table}
+        # SQL
+        sql_query = data["sql_query"]
+        
+        return Output(
+            status = 200,
+            success = True,
+            retCd = 200,
+            message="질답성공",
+            body = {"result": result, "raw_data": raw_data, "SQL": sql_query}
+        )
+        
     except Exception as e:
+        print(traceback.format_exc())
         raise HTTPException(status_code=500, detail=f"Error processing input: {str(e)}")
 
 import json
