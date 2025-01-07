398c9bc435b0de7da67aa46b7a12edf646226f38
lee.jihyeong <lee.jihyeong0823@gmail.com>
2025-01-07 17:13:26 +0900

    테스트 케이스 추가와 페이로드 수정
    last data를 사용하는 코드를 추가해서 테스트 케이스를 2개로 생성
    첫 번째 케이스는 이전과 유사, 두 번째 케이스는 첫 번째 케이스의 결과를 last data로 받는 걸 가정하여 생성, api수정에 따른 페이로드 수정

--- front_test/test.py
+++ front_test/test.py
@@ -5,16 +5,60 @@ import time
 def test_korean_questions():
     # 테스트할 한글 질문들
     questions = [
-        "2024년 상반기 국민은행 월간 거래내역 정리해줘"
+        "2024년 웹케시 월간 거래내역 정리해줘"
     ]
-
+    temp = ""
     for question in questions:
         print(f"\n테스트 질문: {question}")
 
         payload = {
             "user_question": question,
-            "user_id": "default_user"
+            "user_id": "default_user",
+            "last_question":"",
+            "last_answer":"",
+            "last_sql_query":"",
+            "session_id": "default_session"
         }
+
+        try:
+            response = requests.post(
+                "http://localhost:8000/process",
+                headers={"Content-Type": "application/json; charset=utf-8"},
+                json=payload
+            )
+            
+            print("상태 코드:", response.status_code)
+            print("응답 결과:", json.dumps(response.json(), ensure_ascii=False, indent=2))
+            
+        except Exception as e:
+            print("오류 발생:", str(e))
+
+        time.sleep(2)  # 요청 간 간격
+        # 테스트할 한글 질문들
+    
+    questions = [
+        "오늘은 어때?"
+    ]
+    temp = json.loads(json.dumps(response.json(), ensure_ascii=False, indent=2))
+
+    for question in questions:
+        print(f"\n테스트 질문: {question}")
+        try:
+            payload = {
+                "user_question": question,
+                "user_id": "default_user",
+                "last_question": temp.get("body").get("result").get("question"),
+                "last_answer": temp.get("body").get("result").get("answer"),
+                "last_sql_query": temp.get("body").get("SQL"),
+                
+            }
+        except Exception as E:
+            print("Before process fails.")
+            payload = {
+                "user_question": question,
+                "user_id": "default_user",
+                "session_id": "default_session"
+            }
 
         try:
             response = requests.post(
