a1c7c062c4e9abf5fc7bcdc589f58705f6903ae9
KimGoon <kitae.kim@daquv.com>
2025-01-15 11:54:13 +0900

    main.py 실행 시 외부에서 host/port/reload 옵션 조절할 수 있도록 수정

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import asyncio
-import uvicorn
+import argparse
 import logging
+
+import uvicorn
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
 from api.api import api
 from api.data_api import data_api
 from api.llmadmin_api import llmadmin_api
-from fastapi.middleware.cors import CORSMiddleware
-from datetime import datetime
+
 
 logging.basicConfig(
     level=logging.INFO,
@@ -14,6 +16,13 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 logger = logging.getLogger(__name__)
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="FastAPI Uvicorn Server")
+    parser.add_argument("--host", type=str, default="0.0.0.0", help="Host to bind")
+    parser.add_argument("--port", type=int, default=8000, help="Port to bind")
+    parser.add_argument("--reload", action="store_true", help="Enable reload")
+    return parser.parse_args()
 
 app = FastAPI()
 
@@ -31,9 +40,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(api)
+app.include_router(api, prefix="")
 app.include_router(data_api, prefix="/data")
 app.include_router(llmadmin_api, prefix="/llmadmin")
+
 
 if __name__ == "__main__":
     # 이벤트 루프 생성
@@ -52,11 +62,13 @@ if __name__ == "__main__":
         )
     '''
 
+    args = parse_arguments()
+
     config = uvicorn.Config(
         "main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=False,
+        host=args.host,
+        port=args.port,
+        reload=args.reload,
         log_level="info"
     )
 
